4539be79cbfdb9fbc5c45260cb68739e1f75ff49
Kyle <kylejking@gmail.com>
2014-10-12 16:04:35 -0600

    Call the original initialize

--- lib/attr_defaults.rb
+++ lib/attr_defaults.rb
@@ -1,39 +1,41 @@
 module AttrDefaults
-	module Initialize
-		def initialize *args, &block
-			defaults = class << self.class
-				@attr_defaults
-			end			
-			
-			unless defaults.nil?
-				defaults.each do |attrs,default|
-					attrs.each do |attr|
-						instance_variable_set :"@#{attr}", default.call
-					end
-				end
-			end
-		end
-	end
+  module Initialize
+    def initialize *args, &block
+      defaults = class << self.class
+        @attr_defaults
+      end      
+      
+      unless defaults.nil?
+        defaults.each do |attrs,default|
+          attrs.each do |attr|
+            instance_variable_set :"@#{attr}", default.call
+          end
+        end
+      end
+      
+      super *args, &block
+    end
+  end
 
-	module_function
+  module_function
 
-	def included klass
-		klass.prepend Initialize
-		
-		class << klass
-			[:attr, :attr_writer, :attr_reader, :attr_accessor].each do |method|
-				define_method method do |*attrs, &block|
-					super *attrs
-				
-					unless block.nil?
-						defaults = class << self
-							@attr_defaults ||= {}
-						end
-				
-						defaults[attrs] = block
-					end
-				end
-			end
-		end
-	end
+  def included klass
+    klass.prepend Initialize
+    
+    class << klass
+      [:attr, :attr_writer, :attr_reader, :attr_accessor].each do |method|
+        define_method method do |*attrs, &block|
+          super *attrs
+        
+          unless block.nil?
+            defaults = class << self
+              @attr_defaults ||= {}
+            end
+        
+            defaults[attrs] = block
+          end
+        end
+      end
+    end
+  end
 end
